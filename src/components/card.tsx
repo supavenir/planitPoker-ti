1c742ce5bbbdf8aba01868b4e631f8e2d1f3020b
Nathannicolle <nathan-nicolle@orange.fr>
2023-05-22 22:50:17 +0200

    create card component
    + vote route
    
    + adding node_modules and .next folders to gitignore

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,7 +1,7 @@
 export default function Card() {
     return (
         <div>
-            <p>Test</p>
+            <p>Test card</p>
         </div>
     )
 }
--- src/pages/vote/vote.tsx
+++ src/pages/vote/vote.tsx
@@ -2,7 +2,7 @@ import Card from '@/components/card';
 export default function Vote() {
     return (
         <div>
-            <h1>Vote</h1>
+            <h1>Page vote</h1>
             <Card></Card>
         </div>
     );
