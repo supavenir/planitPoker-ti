3f12cc4fabf61e542d0284fb516c9f5589ec0743
Fabien Barré <89039880+fbarre61@users.noreply.github.com>
2023-05-23 22:24:22 +0200

    Ajout du formulaire sur la page rooms

--- src/pages/rooms.tsx
+++ src/pages/rooms.tsx
@@ -1,13 +1,16 @@
-import {useRouter} from "next/router";
 import UIList from "@/components/UIList";
 import RoomService, {Room} from "@/pages/api/Room";
+import Form from "@/components/Form";
+import {useState} from "react";
 
 function RoomsPage({rooms}:{rooms:Room[]}) {
-
+    const [listItems, setListItems] = useState(rooms);
+    console.log(listItems)
   return (
     <div>
       <h1>Rooms</h1>
-      <UIList list={rooms} callback={(room)=>room.name}></UIList>
+        <UIList list={listItems} callback={(room)=>room.name}></UIList>
+        <Form listItems={listItems} setListItems={setListItems}></Form>
     </div>
   );
 }
@@ -20,6 +23,8 @@ let getServerSideProps = async () => {
         }
     }
 };
+
+
 export {getServerSideProps};
 
 export default RoomsPage;
