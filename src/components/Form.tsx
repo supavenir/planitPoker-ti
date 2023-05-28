5b8667cd2c88349d69288240be15881aaa8a57c1
Fabien Barré <barre.fabien61200@gmail.com>
2023-05-28 21:45:07 +0200

    Ajout du champ description et modification liste Suites

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,40 +1,47 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import RoomService, {Room} from "@/pages/api/Room";
 import SuiteService, {Suite} from "@/pages/api/Suite";
 
-
 function Form({listItems, setListItems}:{listItems:Room[], setListItems:Function}) {
     const [roomName, setRoomName] = useState('');
-    let suiteList : Suite[] = [];
-    SuiteService.getAll().then((stories) => {
-        suiteList = stories;
-        suiteList.forEach((story) => {
-            console.log(story);
+    const [selectedSuite, setSelectedSuite] = useState(0);
+    const [suiteList, setSuiteList] = useState<Suite[]>([]);
+    const [roomDescription, setRoomDescription] = useState('');
+    useEffect(() => {
+        SuiteService.getAll().then((stories) => {
+            setSuiteList(stories);
         });
-    });
+    }, []);
 
     return (
         <form onSubmit={async (e)=>{
             e.preventDefault();
-            const newRoom = {
+            let newRoom = {
                 name: roomName,
-                description: "oui",
+                description: roomDescription,
                 points: 1,
                 connectedUsers: "[]",
                 idOwner: 1,
-                idSuite: 1,
+                idSuite: selectedSuite,
                 idTeam: 1
             };
-            await RoomService.add(newRoom).then(() => {
-                setListItems([...listItems, newRoom]);
-            });
+            if(newRoom.idSuite == 0 || newRoom.idSuite == null) {
+                alert("Veuillez sélectionner une suite");
+            }else {
+                await RoomService.add(newRoom).then(() => {
+                    setListItems([...listItems, newRoom]);
+                }).catch((err) => {
+                    alert(err);
+                });
+            }
         }}>
-            <input type="text" placeholder="Nom" onChange={(e) => setRoomName(e.target.value)} />
+            <input type="text" placeholder="Nom" required onChange={(e) => setRoomName(e.target.value)} />
+            <input type="text" placeholder="Description" required onChange={(e) => setRoomDescription(e.target.value)} />
 
-            <select name="stories" id="stories">
-                <option value="">Sélectionnez une suite</option>
+            <select name="stories" id="stories" required onChange={(e) => setSelectedSuite(parseInt(e.target.value))}>
+                <option value="0">Sélectionnez une suite</option>
                 {suiteList.map((suite) => (
-                    <option key={suite.name} value={suite.name}>
+                    <option key={suite.name} value={suite.id}>
                         {suite.name}
                     </option>
                 ))}
